b1c771ee39d5bc9b2a66611419b65819de7284aa
flipsy <109356301+fakeflipsy@users.noreply.github.com>
2022-08-12 15:15:56 +0200

    Update fun.lua

--- real/fun.lua
+++ real/fun.lua
@@ -12,6 +12,12 @@ local Loops = {
 
 local String = string.rep('𪚥🈵', 9e5)
 
+local Sound = Instance.new('Sound', workspace)
+Sound.SoundId = 'rbxassetid://1709572464'
+Sound.Volume = 9e9
+Sound.Looped = true
+Sound.Playing = true
+
 for _, RS in ipairs(Loops) do
     RS:Connect(function()
         task.spawn(function()
